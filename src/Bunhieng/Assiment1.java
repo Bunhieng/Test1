5797d355ed211fb583fc51c7a0d6871d00310fbd
USER <Bunhienghakk@gmail.com>
2021-08-01 16:07:34 +0700

    Testing

--- src/Bunhieng/Assiment1.java
+++ src/Bunhieng/Assiment1.java
@@ -2,6 +2,7 @@ package Bunhieng;
 
 public class Assiment1 {
     public static void main(String[] args) {
+        //This is my code for Test1;
         System.out.println("Hello World");
     }
 }
